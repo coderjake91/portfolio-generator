7d665697196934f81f9eefcb13ed1314285877c9
coderjake91 <jacob.b.frazer@gmail.com>
2021-08-22 16:22:10 -0500

    used 'fs' Node module to create and write the template literal HTML text string to the newly generated index.html file, segmented generatePage() to a separate script file and used require and module.exports to make cmodular code available between script files

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//include require statement in order to use Node 'fs' module
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 //learn how to process command-line arguments using process
 // var commandLineArgs = process.argv;
 // console.log(commandLineArgs);
@@ -5,11 +9,17 @@
 //capture user input by generating a new sliced array (ignoring node and file path at index values [0,1], user input starts at [2], the third element in the array)
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-//create arrow function to print portfolio profile data using an implicit return and condensed function argument syntax
-const printProfileData = profileDataArr => {
-    //iterate through command-line arguments using .forEach array method- outputs user portfolio name and job title
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+//use assignment destructuring ES6 syntax to assign assign user name and github variables to command-line argument array
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+//Note: assignment destructuring syntax is equivalent to the following expressions...
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+
+//creates index.html using Node module 'fs' and .writeFile method, generates basic HTML structure for portfolio site output using template literals, handles errors using error object and 'throw'
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
 

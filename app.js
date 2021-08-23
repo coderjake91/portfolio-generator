b3c7af1c7405ddb1cd0e50a9d351340791af3761
coderjake91 <jacob.b.frazer@gmail.com>
2021-08-23 00:30:28 -0500

    add .gitignore to root directory with node_modules ignored, add in require statement for inquirer npm module, add in inital inquirer prompt code

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
+const inquirer = require('inquirer');
 //include require statement in order to use Node 'fs' module
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-//learn how to process command-line arguments using process
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
+// const pageHTML = generatePage(name, github);
 
-//capture user input by generating a new sliced array (ignoring node and file path at index values [0,1], user input starts at [2], the third element in the array)
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// //creates index.html using Node module 'fs' and .writeFile method, generates basic HTML structure for portfolio site output using template literals, handles errors using error object and 'throw'
+// fs.writeFile('./index.html', pageHTML , err => {
+//     if(err) throw new Error(err);
 
-//use assignment destructuring ES6 syntax to assign assign user name and github variables to command-line argument array
-const [name, github] = profileDataArgs;
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-//Note: assignment destructuring syntax is equivalent to the following expressions...
-//const name = profileDataArgs[0];
-//const github = profileDataArgs[1];
-
-//creates index.html using Node module 'fs' and .writeFile method, generates basic HTML structure for portfolio site output using template literals, handles errors using error object and 'throw'
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if(err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
+//utilize inquirer to prompt user with a series of questions to fill out the portfolio information
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
